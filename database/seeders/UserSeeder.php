f10c68146ebfd1498ccddd16c1c693b4762ca433
manikandansubramani-censo <manikandan52357@gmail.com>
2025-07-04 20:38:57 +0530

    seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash as FacadesHash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+        'name' => 'Admin',
+        'email' => 'admin@gmail.com',
+        'password' => FacadesHash::make('admin123'), // Hashed password
+    ]);
+    }
+}
